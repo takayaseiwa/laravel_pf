03b9371cf927a5e6ea068af637225f94f46965b2
Takaya Seiwa <ljks1021@gmail.com>
2021-12-03 23:59:59 +0900

    “マイページ作成7”

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -24,7 +24,8 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:25|unique:users'
         ];
     }
 }
