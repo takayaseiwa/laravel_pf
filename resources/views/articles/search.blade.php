adc459b8412b8aa8ca868015534f1939dfdd8706
Takaya Seiwa <ljks1021@gmail.com>
2021-12-08 22:56:11 +0900

    検索ページ6

--- resources/views/articles/search.blade.php
+++ resources/views/articles/search.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-
+                        <div class="form-group mb-4">
+                            <p>カテゴリー<span class="text-danger">(※)</span></p>
+                            <select id="category_id" name="category_id" class="form-control">
+                                @foreach($categories as $id => $name)
+                                    <option value="{{ $id }}">
+                                        {{ $name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
 
 @endsection
