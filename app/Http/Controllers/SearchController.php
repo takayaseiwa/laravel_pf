dfe9eeffbfe6d0b57079c6a9085c36606a9fd85b
Takaya Seiwa <ljks1021@gmail.com>
2021-12-11 19:17:54 +0900

    “検索ページ10”

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 use App\Models\Category;
 
@@ -18,7 +19,31 @@ class SearchController extends Controller
 
     public function search(Request $request)
     {
-        dd($request->search);
+        $searchWord = $request->input('searchWord');
+        $categoryId = $request->input('categoryId');
+
+        $query = Article::query();
+        
+        //フリーワード検索
+        if (isset($searchWord)) {
+            $query->where('title', 'like', '%' . self::escapeLike($searchWord) . '%')
+                ->orWhere('summary', 'like', '%' . self::escapeLike($searchWord) . '%');
+        }
+        //カテゴリー検索
+        if (isset($categoryId)) {
+            $query->where('category_id' , $categoryId);
+        }
+
+        $products = $query->orderBy('title' , 'asc')->paginate(9);
+
+        $category = new Category;
+        $categories = $category->getLists();
+
+        return view('articles.search', compact('article','categories','searchWord','categoryId'));
+    }
+
+    public static function escapeLike($str){
+        return str_replace(['\\', '%', '_'], ['\\\\', '\%', '\_'], $str);
     }
 
 }
